f150d53ca63670e6678945b98057f091e3c38e25
acfoard <acfoard@gmail.com>
2019-03-03 17:31:49 -0500

    db changes

--- config/keys.js
+++ config/keys.js
@@ -1,1 +1,1 @@
-module.exports = "mongodb://kudoUser1:funfunfun@cluster0-shard-00-00-dofrr.mongodb.net:27017,cluster0-shard-00-01-dofrr.mongodb.net:27017,cluster0-shard-00-02-dofrr.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+module.exports = "mongodb+srv://kudoUser1:funfunfun@cluster0-dofrr.mongodb.net/tinyImprovements?retryWrites=true"
